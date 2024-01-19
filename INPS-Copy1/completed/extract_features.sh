a624f96c38beca8cf720d1e0c061ef2f63df654e
Zachary Greenberg <zfg2013@c1100a-s29.ufhpc>
2024-01-19 10:41:15 -0500

    Amanda's files added

---
+++ INPS-Copy1/completed/extract_features.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# NOTE: Check your bash location and replace the line above with it. Keep the #!
+# NOTE: You can run this by changing permissions so it's executable OR by running bash <scriptname> 
+
+FEATURE=raw_features.csv
+
+#extract from completed computes
+
+for f in ~/blue_zfg2013/INPS/completed/*.out
+do 
+# echo "Processing $f file.."
+FILENAME=$(basename -- "$f")
+{
+  grep 'NAME' "$f";
+  grep 'FINAL SINGLE POINT ENERGY' $f | tail -n1;
+  grep 'GEPOL Volume' $f | tail -n1 >> $FEATURE;
+  grep 'GEPOL Surface-area' $f | tail -n1 >> $FEATURE;
+  grep 'Rotational constants in MHz' $f | tail -n1 >> $FEATURE;
+  grep 'Total Dipole Moment' $f | tail -n1 >> $FEATURE;
+} >> $FEATURE
+
+done
